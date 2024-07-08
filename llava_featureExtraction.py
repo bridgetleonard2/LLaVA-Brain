ccf00ce0d69d1221d88015916c3ac0dd2b4c9dd4
Bridget Leonard <leonardbridget01@gmail.com>
2024-07-08 15:03:14 -0700

    add feature extraction test

---
+++ llava_featureExtraction.py
@@ -0,0 +1,54 @@
+# confirm that feature extraction works with llava
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration, BitsAndBytesConfig
+import torch
+from PIL import Image
+import requests
+
+model_id = 'llava-hf/llava-v1.6-34b-hf'
+processor = LlavaNextProcessor.from_pretrained(model_id)
+
+# specify how to quantize the model
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = LlavaNextForConditionalGeneration.from_pretrained(model_id, quantization_config=quantization_config, device_map="auto")
+
+features = {}
+
+
+def get_features(name):
+    def hook(model, input, output):
+        # detached_outputs = [tensor.detach() for tensor in output]
+        last_output = output[-1].detach()
+        features[name] = last_output  # detached_outputs
+    return hook
+
+
+layer = model.multi_modal_projector.linear_2.register_forward_hook(get_features('mm_proj_L2'))
+
+# prepare image and text prompt, using the appropriate prompt template
+url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
+image = Image.open(requests.get(url, stream=True).raw)
+
+# llava-v1.6-34b-hf requires the following format:
+# "<|im_start|>system\nAnswer the questions.<|im_end|><|im_start|>user\n<image>\nWhat is shown in this image?<|im_end|><|im_start|>assistant\n"
+prompt = "<|im_start|>system\nAnswer the questions.<|im_end|><|im_start|>user\n<image>\nWhat is shown in this image?<|im_end|><|im_start|>assistant\n"
+
+inputs = processor(prompt, image, return_tensors="pt").to('cuda')
+
+# autoregressively complete prompt
+output = model.generate(**inputs, max_new_tokens=100)
+
+print(processor.decode(output[0], skip_special_tokens=True))
+
+# Extracted features:
+mm_feat = features['mm_proj_L2']  # This will contain the extracted features from the specified layer
+
+# print some of mm_feat
+print(mm_feat[:10])
+
+layer.remove()  # Remove the hook after extracting features
