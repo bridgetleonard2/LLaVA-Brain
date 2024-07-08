1c264a7fc11f827eaf5946a8ec993c953c683972
Bridget Leonard <leonardbridget01@gmail.com>
2024-07-08 15:44:34 -0700

    feat extract works, add example output

--- llava_featureExtraction.py
+++ llava_featureExtraction.py
@@ -52,3 +52,18 @@ mm_feat = features['mm_proj_L2']  # This will contain the extracted features fro
 print(mm_feat[:10])
 
 layer.remove()  # Remove the hook after extracting features
+
+
+# Works -- Output:
+# What is shown in this image?<|im_start|> assistant
+# The image displays a radar chart, also known as a spider chart, which is a graphical method of displaying multivariate data in the form of a two-dimensional chart of three or more quantitative variables represented on axes starting from the same point.
+
+# In this particular chart, there are several datasets represented by different colors and labels, such as "MM-Vet," "LLaVa-Bench," "Seed-Bench," "MMBench-CN," "
+# tensor([[ 0.0598, -0.0310, -0.2712,  ..., -0.0336, -0.3164,  0.1720],
+#         [-0.1852,  0.1825, -0.3052,  ..., -0.0571, -0.3198,  0.1362],
+#         [ 0.3123, -0.2148, -0.1020,  ..., -0.1548, -0.0385, -0.0592],
+#         ...,
+#         [ 0.2113, -0.2042, -0.1179,  ...,  0.0053, -0.2761, -0.0986],
+#         [-0.3074,  0.0357, -0.1349,  ..., -0.2786, -0.3005,  0.1462],
+#         [-0.5898,  0.4802, -0.5269,  ...,  0.3064,  0.2454,  0.1152]],
+#        device='cuda:0', dtype=torch.float16)
