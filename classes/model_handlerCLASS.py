ac250b8cd4041fe8f339907a1bde000b6deafae2
Bridget Leonard <leonardbridget01@gmail.com>
2024-07-09 11:39:51 -0700

    add model handler class

---
+++ classes/model_handlerCLASS.py
@@ -0,0 +1,50 @@
+import torch
+
+# models
+from transformers import BridgeTowerModel, BridgeTowerProcessor
+from transformers import AutoProcessor
+from transformers import LlavaForConditionalGeneration
+from transformers import BitsAndBytesConfig
+
+
+class ModelHandler:
+    def __init__(self, model_name):
+        if model_name == 'llava':
+            self.model = LlavaForConditionalGeneration
+            self.model_id = "llava-hf/llava-1.5-7b-hf"
+            self.processor = AutoProcessor
+            self.q = BitsAndBytesConfig(
+                                        load_in_4bit=True,
+                                        bnb_4bit_quant_type="nf4",
+                                        bnb_4bit_compute_dtype=torch.float16)
+        elif model_name == 'bridgetower':
+            self.model = BridgeTowerModel
+            self.model_id = "BridgeTower/bridgetower-base"
+            self.processor = BridgeTowerProcessor
+
+    def select_layer(self):
+        self.layer = self.model.multi_modal_projector.linear_2
+
+    def load_model(self):
+        device = torch.device('cuda')
+
+        if self.quant:
+            self.model = self.model.from_pretrained(self.model_id,
+                                                    quantization_config=self.q,
+                                                    device=device)
+        else:
+            self.model = self.model.from_pretrained(self.model_id,
+                                                    device=device)
+
+        self.processor = self.processor.from_pretrained(self.model_id)
+
+        self.features = {}
+
+        def get_features(name):
+            def hook(model, input, output):
+                # detached_outputs = [tensor.detach() for tensor in output]
+                last_output = output[-1].detach()
+                self.features[name] = last_output  # detached_outputs
+            return hook
+
+        self.hook = self.layer.register_forward_hook(get_features('layer'))
---
+++ classes/test_modelHandler.py
@@ -0,0 +1,33 @@
+import model_handlerCLASS
+from PIL import Image
+import requests
+
+model_name = 'llava'
+model_handler = model_handlerCLASS.ModelHandler(model_name)
+model_handler.select_layer()
+model_handler.load_model()
+
+# test feature extraction
+
+# prepare image and text prompt, using the appropriate prompt template
+url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
+image = Image.open(requests.get(url, stream=True).raw)
+
+# llava-v1.6-34b-hf requires the following format:
+# "<|im_start|>system\nAnswer the questions.<|im_end|><|im_start|>user\n<image>\nWhat is shown in this image?<|im_end|><|im_start|>assistant\n"
+prompt = "<|im_start|>system\nAnswer the questions.<|im_end|><|im_start|>user\n<image>\nWhat is shown in this image?<|im_end|><|im_start|>assistant\n"
+
+inputs = model_handler.processor(prompt, image, return_tensors="pt").to('cuda')
+
+# autoregressively complete prompt
+output = model_handler.model.generate(**inputs, max_new_tokens=100)
+
+print(model_handler.processor.decode(output[0], skip_special_tokens=True))
+
+# Extracted features:
+features = model_handler.features['layer']  # This will contain the extracted features from the specified layer
+
+# print some of features
+print(features[:10])
+
+model_handler.hook.remove()  # Remove the hook after extracting features
