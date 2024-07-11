a289d9d64b65ef2931389b18bad38b07e51b867d
Bridget Leonard <leonardbridget01@gmail.com>
2024-07-11 13:29:42 -0700

    try sending features to cpu

--- classes/model_handlerCLASS.py
+++ classes/model_handlerCLASS.py
@@ -46,10 +46,8 @@ class ModelHandler:
         def get_features(name):
             def hook(model, input, output):
                 # detached_outputs = [tensor.detach() for tensor in output]
-                last_output = output[-1].detach()
-                print(last_output)
+                last_output = output[-1].detach().cpu()
                 self.features[name].append(last_output)  # detached_outputs
-                print(self.features)
             return hook
 
         self.hook = self.layer.register_forward_hook(get_features('layer'))
