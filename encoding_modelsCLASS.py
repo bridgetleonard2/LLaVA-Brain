90f98f5c85b73aee2b721f72b3b2b8afbe609bcd
Bridget Leonard <leonardbridget01@gmail.com>
2024-07-15 14:21:22 -0700

    first run through of encoding models done

--- encoding_modelsCLASS.py
+++ encoding_modelsCLASS.py
@@ -4,10 +4,11 @@ from classes import visual_featuresCLASS
 from scipy.signal import resample
 from sklearn.model_selection import check_cv
 from sklearn.preprocessing import StandardScaler
-from sklearn.utils.validation import check_random_state, check_is_fitted, check_array
-from himalaya.backend import set_backend
+from sklearn.utils.validation import (
+    check_random_state, check_is_fitted, check_array)
+from himalaya.backend import set_backend  # type: ignore
 from sklearn.base import BaseEstimator, TransformerMixin
-from himalaya.ridge import RidgeCV
+from himalaya.ridge import RidgeCV  # type: ignore
 from sklearn.pipeline import make_pipeline
 from sklearn import set_config
 
@@ -196,7 +197,7 @@ def remove_run(arrays, index_to_remove):
             if idx != index_to_remove]
 
 
-def set_params(feature_arrays):
+def set_pipeline(feature_arrays):
     run_onsets = []
     current_index = 0
     for arr in feature_arrays:
@@ -215,7 +216,21 @@ def set_params(feature_arrays):
     backend = set_backend("torch_cuda", on_error="warn")
     print(backend)
     alphas = np.logspace(1, 20, 20)
-    return cv, scaler, delayer, backend, alphas
+
+    ridge_cv = RidgeCV(
+                alphas=alphas,
+                cv=cv,
+                solver_params=dict(
+                    n_targets_batch=500, n_alphas_batch=5,
+                    n_targets_batch_refit=100))
+
+    pipeline = make_pipeline(
+        scaler,
+        delayer,
+        ridge_cv,
+    )
+
+    return pipeline, backend
 
 
 def safe_correlation(x, y):
@@ -251,63 +266,118 @@ def calc_correlation(predicted_fMRI, real_fMRI):
 
     return correlation_coefficients
 
-class EncodingModels:
-    def __init__(self, stim_dir, fmri_dir, model_handler):
-        """Initialize the EncodingModels class.
-        stim_dir (str): Path to the directory containing the stimuli.
-        fmri_dir (str): Path to the directory containing the fMRI data.
 
-        len(stim_dir) == len(fmri_dir)
-        Order of files in stim_dir and fmri_dir must match.
+class EncodingModels:
+    def __init__(self, train_stim_dir, train_fmri_dir, model_handler,
+                 test_stim_dir=None, test_fmri_dir=None):
+        """Initialize the EncodingModels class.
+        train_stim_dir (str): Path to the directory with training stimuli.
+        train_fmri_dir (str): Path to the directory with training fMRI data.
+
+        len(train_stim_dir) == len(train_fmri_dir)
+        Order of files in train_fmri_dir and train_fmri_dir must match.
 
         Naming convention for files:
-        stim_dir: 'stim_01', 'stim_02', ...
-        fmri_dir: 'fmri_01', 'fmri_02', ..."""
-        self.stim_dir = stim_dir
-        self.fmri_dir = fmri_dir
+        train_stim_dir: 'stim_01', 'stim_02', ...
+        train_fmri_dir: 'fmri_01', 'fmri_02', ...
+
+        Same rules apply to test data if provided."""
+        self.train_stim_dir = train_stim_dir
+        self.fmri_dir = train_fmri_dir
         self.model_handler = model_handler
+        self.test_stim_dir = test_stim_dir
+        self.test_fmri_dir = test_fmri_dir
 
         # Prep files
-        self.stim_files = os.listdir(stim_dir)
-        self.fmri_files = os.listdir(fmri_dir)
-        if len(self.stim_files) != len(self.fmri_files):
+        self.train_stim_files = os.listdir(train_stim_dir)
+        self.train_fmri_files = os.listdir(train_fmri_dir)
+
+        # Check rules
+        if len(self.train_stim_files) != len(self.train_fmri_files):
             raise ValueError("Length of stim_dir and fmri_dir must be equal.")
-        self.stim_files.sort()
-        self.fmri_files.sort()
+        self.train_stim_files.sort()
+        self.train_fmri_files.sort()
         # check naming convention
-        for stim, fmri in zip(self.stim_files, self.fmri_files):
+        for stim, fmri in zip(self.train_stim_files, self.train_fmri_files):
             if stim.split('_')[1] != fmri.split('_')[1]:
                 raise ValueError(
                     "Naming convention mismatch between stim_dir and fmri_dir."
                     )
 
+        if test_stim_dir and test_fmri_dir:
+            # Prep files
+            self.test_stim_files = os.listdir(test_stim_dir)
+            self.test_fmri_files = os.listdir(test_fmri_dir)
+
+            # Check rules
+            if len(self.test_stim_files) != len(self.test_fmri_files):
+                raise ValueError(
+                    "Length of stim_dir and fmri_dir must be equal.")
+            self.test_stim_files.sort()
+            self.test_fmri_files.sort()
+            # check naming convention
+            for stim, fmri in zip(self.test_stim_files, self.test_fmri_files):
+                if stim.split('_')[1] != fmri.split('_')[1]:
+                    raise ValueError(
+                        "Naming convention mismatch between "
+                        "stim_dir and fmri_dir."
+                        )
+
     def load_fmri(self):
-        self.fmri_arrays = []
-        for fmri_file in self.fmri_files:
-            fmri_path = os.path.join(self.fmri_dir, fmri_file)
+        self.train_fmri_arrays = []
+        for fmri_file in self.train_fmri_files:
+            fmri_path = os.path.join(self.train_fmri_dir, fmri_file)
             fmri_data = np.load(fmri_path)
             fmri_data_clean = remove_nan(fmri_data)
-            new_feat_arrays.append(fmri_data_clean)
-        self.fmri_shape = fmri_data.shape
+            self.train_fmri_arrays.append(fmri_data_clean)
+        self.train_fmri_shape = fmri_data.shape
+
+        if self.test_fmri_files:
+            self.test_fmri_arrays = []
+            for fmri_file in self.test_fmri_files:
+                fmri_path = os.path.join(self.test_fmri_dir, fmri_file)
+                fmri_data = np.load(fmri_path)
+                fmri_data_clean = remove_nan(fmri_data)
+                self.test_fmri_arrays.append(fmri_data_clean)
+            self.test_fmri_shape = fmri_data.shape
 
     def load_features(self):
-        self.feature_arrays = []
-        for stim_file in self.stim_files:
-            stim_path = os.path.join(self.stim_dir, stim_file)
+        self.train_feature_arrays = []
+        for stim_file in self.train_stim_files:
+            stim_path = os.path.join(self.train_stim_dir, stim_file)
             visual_features = visual_featuresCLASS.VisualFeatures(
                 stim_path, self.model_handler)
             visual_features.load_image()
             stim_features = visual_features.get_features()
 
             # Only resample features if dimensions don't match fmri
-            if stim_features.shape[0] != self.fmri_shape[0]:
-                stim_features_resampled = resample_to_acq(stim_features,
-                                                          self.fmri_shape)
+            if stim_features.shape[0] != self.train_fmri_shape[0]:
+                stim_features_resampled = resample_to_acq(
+                    stim_features, self.train_fmri_shape)
             else:
                 stim_features_resampled = stim_features
-            self.feature_arrays.append(stim_features_resampled)
+            self.train_feature_arrays.append(stim_features_resampled)
+
+        if self.test_stim_files:
+            self.test_feature_arrays = []
+            for stim_file in self.test_stim_files:
+                stim_path = os.path.join(self.test_stim_dir, stim_file)
+                visual_features = visual_featuresCLASS.VisualFeatures(
+                    stim_path, self.model_handler)
+                visual_features.load_image()
+                stim_features = visual_features.get_features()
+
+                # Only resample features if dimensions don't match fmri
+                if stim_features.shape[0] != self.test_fmri_shape[0]:
+                    stim_features_resampled = resample_to_acq(
+                        stim_features, self.test_fmri_shape)
+                else:
+                    stim_features_resampled = stim_features
+                self.test_feature_arrays.append(stim_features_resampled)
 
     def evaluate(self):
+        """Evaluate the encoding models using leave-one-run-out
+        cross-validation."""
         self.correlations = []
 
         for i in range(len(self.feature_arrays)):
@@ -318,25 +388,12 @@ class EncodingModels:
 
             print("X_train shape", X_train.shape)
 
-            cv, scaler, delayer, backend, alphas = set_params(new_feat_arrays)
-            X_train = X_train.astype(np.float32)
-
-            ridge_cv = RidgeCV(
-                alphas=alphas,
-                cv=cv,
-                solver_params=dict(
-                    n_targets_batch=500, n_alphas_batch=5,
-                    n_targets_batch_refit=100))
-
-            pipeline = make_pipeline(
-                scaler,
-                delayer,
-                ridge_cv,
-            )
+            pipeline, backend = set_pipeline(new_feat_arrays)
 
             set_config(display='diagram')  # requires scikit-learn 0.23
             pipeline
 
+            X_train = X_train.astype(np.float32)
             _ = pipeline.fit(X_train, Y_train)
 
             coef = pipeline[-1].coef_
@@ -369,3 +426,64 @@ class EncodingModels:
             print("Max correlation:", np.nanmax(test_correlations))
 
             self.correlations.append(test_correlations)
+
+    def build(self, alignment=False):
+        """Build the encoding model."""
+        if alignment:
+            # replace with actual later***
+            align_matrix = np.random.randn(self.train_fmri_shape[1],
+                                           self.test_fmri_shape[1])
+            transformed_features = []
+            for i in range(len(self.train_feature_arrays)):
+                transformed_features.append(np.dot(
+                    self.train_feature_arrays[i],
+                    align_matrix))
+            self.train_feature_arrays = transformed_features
+
+        print("Building encoding model using all training data")
+        X_train = np.vstack(self.feature_arrays)
+        Y_train = np.vstack(self.fmri_arrays)
+
+        pipeline, backend = set_pipeline(self.feature_arrays)
+
+        set_config(display='diagram')  # requires scikit-learn 0.23
+        pipeline
+
+        X_train = X_train.astype(np.float32)
+        _ = pipeline.fit(X_train, Y_train)
+
+        coef = pipeline[-1].coef_
+        coef = backend.to_numpy(coef)
+        print("(n_delays * n_features, n_voxels) =", coef.shape)
+        # Get encoding model from coefficients
+        # Regularize coefficients
+        coef /= np.linalg.norm(coef, axis=0)[None]
+
+        delayer = pipeline.named_steps['delayer']
+        coef_per_delay = delayer.reshape_by_delays(coef, axis=0)
+        print("(n_delays, n_features, n_voxels) =", coef_per_delay.shape)
+
+        average_coef = np.mean(coef_per_delay, axis=0)
+        print("(n_features, n_voxels) =", average_coef.shape)
+
+        self.encoding_model = average_coef
+
+    def predict(self):
+        """Predict fMRI data using the encoding model."""
+        self.predictions = []
+        for i in range(len(self.test_feature_arrays)):
+            X_test = self.test_feature_arrays[i]
+            Y_pred = np.dot(X_test, self.encoding_model)
+            self.predictions.append(Y_pred)
+
+    def correlate(self):
+        """Calculate the correlation between predicted and actual fMRI data."""
+        self.correlations = []
+        for i in range(len(self.predictions)):
+            test_correlations = calc_correlation(self.predictions[i],
+                                                 self.test_fmri_arrays[i])
+            self.correlations.append(test_correlations)
+            # Take the mean of the correlations
+            self.mean_correlations = np.nanmean(np.stack((test_correlations)),
+                                                axis=0)
+            print("Max correlation:", np.nanmax(self.mean_correlations))
