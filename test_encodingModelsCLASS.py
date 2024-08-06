0a2c342a84615d1e87a6b5e9de49bfa27d194993
Bridget Leonard <leonardbridget01@gmail.com>
2024-08-06 09:56:17 -0700

    try bridgetower pipeline again

--- test_encodingModelsCLASS.py
+++ test_encodingModelsCLASS.py
@@ -5,7 +5,7 @@ model_name = 'llava'
 model_handler = model_handlerCLASS.ModelHandler(model_name)
 model_handler.load_model()
 
-test_type = "predcorr"
+test_type = "eval"
 data_dir = "../bridgetower-brain/data/encodingModels_pipeline"
 
 if test_type == "eval":
