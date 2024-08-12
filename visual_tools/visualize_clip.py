f2f90ded7c92bd6c886585932e1428a547393f84
Bridget Leonard <leonardbridget01@gmail.com>
2024-08-12 15:00:52 -0700

    i did it!

--- visual_tools/visualize_clip.py
+++ visual_tools/visualize_clip.py
@@ -21,7 +21,8 @@ transform_file = os.path.join('nsd_pycortex_db', subject, 'transforms',
                               transform_name, 'matrices.xfm')
 
 # Create the Volume object
-prediction_data = np.load('results/vision_tower/subj01.npy')
+prediction_data = np.load(
+    'results/multi-modal_projector/pred_correlations.npy')
 
 # take absolute value of prediction data
 prediction_data = np.abs(prediction_data)
@@ -29,13 +30,15 @@ prediction_data = np.abs(prediction_data)
 # keep only results over 0.05
 prediction_data[prediction_data < 0.05] = 0
 
-brain_dims = (83, 104, 81)
+brain_dims = (81, 104, 83)
 prediction_3d = np.zeros(brain_dims)
 mask = np.load("visual_tools/cortical_mask_subj01.npy")
-mask = np.transpose(mask, (2, 1, 0))
 print("Mask shape:", mask.shape)
 print("Prediction shape:", prediction_3d.shape)
 prediction_3d[mask] = prediction_data
+
+prediction_3d = np.transpose(prediction_3d, (2, 1, 0))
+print("Prediction shape:", prediction_3d.shape)
 
 # Flatten the data to 2D (example with max projection)
 flat_prediction = np.max(prediction_3d, axis=2)
@@ -43,7 +46,7 @@ flat_prediction = np.max(prediction_3d, axis=2)
 vol = cortex.Volume(prediction_3d, subject, transform_name, cmap="inferno")
 
 # Create and display the flatmap
-output_png = 'results/vision_tower/subj01_flatmap.png'
+output_png = 'results/multi-modal_projector/subj01_8515_flatmap.png'
 fig = cortex.quickflat.make_png(output_png, vol, with_colorbar=True,
                                 bgcolor='white')
 
