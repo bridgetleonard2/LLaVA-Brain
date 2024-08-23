7f0d52bbc90be07d22d9ee4e8083610de0e83774
Bridget Leonard <leonardbridget01@gmail.com>
2024-08-23 11:16:46 -0700

    add in all face/land results

--- visual_tools/visualize_clip.py
+++ visual_tools/visualize_clip.py
@@ -15,14 +15,14 @@ filestore_path = cortex.database.default_filestore
 assert filestore_path == 'nsd_pycortex_db', f"cortex is using {filestore_path}"
 
 # Check if the transformation file exists
-subject = 'subj01'
+subject = 'subj05'
 transform_name = 'func1pt8_to_anat0pt8_autoFSbbr'
 transform_file = os.path.join('nsd_pycortex_db', subject, 'transforms',
                               transform_name, 'matrices.xfm')
 
 # good correlation mask
 corr_data = np.load(
-    'results/multi-modal_projector/subj01_8515_r_squared.npy'
+    'results/multi-modal_projector/subj05_8515_r_squared.npy'
     )
 print("Correlation data shape:", corr_data.shape)
 mask = np.load(f"visual_tools/cortical_mask_{subject}.npy")
@@ -40,26 +40,23 @@ print("Correlation shape:", corr_3d.shape)
 
 # # # # Create the Volume object
 # prediction_data = np.load(
-#     'results/multi-modal_projector/subj01_85_face_predictions.npy'
+#     'results/multi-modal_projector/subj05_85_land_predictions.npy'
 #     )
 
-face_data = np.load('results/multi-modal_projector/subj01_85_face_predictions.npy')
-landscape_data = np.load('results/multi-modal_projector/subj01_85_land_predictions.npy')
+face_data = np.load(
+    'results/multi-modal_projector/subj05_85_face_predictions.npy')
+landscape_data = np.load(
+    'results/multi-modal_projector/subj05_85_land_predictions.npy')
 
 # # normalize data before taking difference
-face_data = (face_data - np.mean(face_data)) / np.std(face_data)
-landscape_data = (landscape_data - np.mean(landscape_data)) / np.std(landscape_data)
+# face_data = (face_data - np.mean(face_data)) / np.std(face_data)
+# landscape_data = (
+#     landscape_data - np.mean(landscape_data)) / np.std(landscape_data)
 
 print("Max diff", np.max(face_data - landscape_data))
 print("Min diff", np.min(face_data - landscape_data))
 print("Mean diff", np.mean(face_data - landscape_data))
 prediction_data = face_data - landscape_data
-
-# # take absolute value of prediction data
-# # prediction_data = np.abs(prediction_data)
-
-# # # only take positive values above 0.05
-# # prediction_data = np.where(prediction_data > 0.3, prediction_data, 0)
 
 prediction_3d = np.zeros(brain_dims)
 print("Mask shape:", mask.shape)
@@ -75,15 +72,17 @@ print("Prediction shape:", prediction_3d.shape)
 # # Flatten the data to 2D (example with max projection)
 # flat_prediction = np.max(prediction_3d, axis=2)
 
+# use inferno if plotting corr/r2, use rdbu if plotting prediction
 vmin = prediction_3d.min()
 vmax = prediction_3d.max()
-vol = cortex.Volume(prediction_3d, subject, transform_name, cmap="inferno",  # "RdBu_r", "hot",
+vol = cortex.Volume(prediction_3d, subject, transform_name,
+                    cmap="RdBu_r",  # "inferno"
                     vmin=vmin, vmax=vmax
                     )
 # cmap="inferno")
 
 # Create and display the flatmap
-output_name = 'vision_subj01_85_faceminusland'
+output_name = 'vision_subj05_85_faceminusland'
 output_png = f'results/multi-modal_projector/{output_name}.png'
 
 fig = cortex.quickflat.make_png(output_png, vol, with_colorbar=True,
